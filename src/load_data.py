c441e2efd1e87cf1adfad1e1f6ecc4ec38b1c9b6
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-08-07 23:14:59 +0530

    load_combined + binary target helper

--- src/load_data.py
+++ src/load_data.py
@@ -16,3 +16,23 @@ def load_white():
     """Load the UCI white wine quality csv as a DataFrame."""
     path = os.path.join(DATA_DIR, "winequality-white.csv")
     return pd.read_csv(path, sep=";")
+
+
+def load_combined():
+    """Load both red and white wines, tag with a wine_type column."""
+    red = load_red()
+    red["wine_type"] = "red"
+    white = load_white()
+    white["wine_type"] = "white"
+    return pd.concat([red, white], axis=0, ignore_index=True)
+
+
+def make_binary_target(df, threshold=7):
+    """Turn the quality column into a 0/1 'good wine' label.
+
+    Quality >= threshold is considered good. The UCI dataset is heavily
+    skewed toward 5 and 6 so picking 7 gives us a ~15% positive class.
+    """
+    df = df.copy()
+    df["good"] = (df["quality"] >= threshold).astype(int)
+    return df
