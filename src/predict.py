ecfd27ee7abfb585422bd454daf39171fe78ac78
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-08-17 10:28:09 +0530

    tweak predict - batch + proba helpers

--- src/predict.py
+++ src/predict.py
@@ -19,3 +19,17 @@ def predict_one(pipe, sample):
     row = np.array([[sample[f] for f in FEATURES]])
     label = int(pipe.predict(row)[0])
     return label
+
+
+def predict_batch(pipe, samples):
+    """Same idea but for a list of dicts."""
+    rows = np.array([[s[f] for f in FEATURES] for s in samples])
+    return [int(x) for x in pipe.predict(rows)]
+
+
+def predict_proba_one(pipe, sample):
+    """Probability of the positive (good) class for a single sample."""
+    row = np.array([[sample[f] for f in FEATURES]])
+    proba = pipe.predict_proba(row)[0]
+    # Return P(good=1) which is the second column under sklearn's convention.
+    return float(proba[1])
